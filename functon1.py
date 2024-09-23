61f425afa0f5b26e0c4e52043e5fd223ecb01292
kys999 <kys998@naver.com>
2024-09-23 16:23:09 +0900

    두번째

--- functon1.py
+++ functon1.py
@@ -10,4 +10,19 @@ print(retValue)
 def swap(x,y):
     return y,x
 
+
 print(swap(3,4))
+
+def times(a=10, b=20):
+    return a*b
+
+print(times())
+print(times(5))
+print(times(5,6))
+
+x=5
+def func1(a):
+    return a+x
+print(func1(1))
+
+print(func2(1))
