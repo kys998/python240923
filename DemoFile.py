ffca549e8bf6dc7128e6540ecd6063281928a208
kys999 <kys998@naver.com>
2024-09-24 13:40:13 +0900

    file read write exe

---
+++ DemoFile.py
@@ -0,0 +1,10 @@
+# DemoFile.py
+
+f=open("demo.txt", "wt", encoding="utf-8")
+f.write("첫번째\n두번째\n세번째\n")
+f.close
+
+f=open("demo.txt", "rt", encoding="utf-8")
+result=f.read()
+f.close
+print(result)
