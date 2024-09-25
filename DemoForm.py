4739be8a2d058521cac7f2ee7d9c3b110219b956
kys999 <kys998@naver.com>
2024-09-25 16:52:38 +0900

    3일차 마지막

---
+++ DemoForm.py
@@ -0,0 +1,20 @@
+# DemoForm.py
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+form_class = uic.loadUiType("DemoForm.ui")[0]
+
+class DemoForm(QDialog, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.label.setText("첫번째 화면 출력")
+
+# 진입점 체크
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = DemoForm()
+    myWindow.show()
+    app.exec_()
